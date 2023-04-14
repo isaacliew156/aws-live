c378bf18a8994b44617fb437f0ac28a43e2971e2
Isaac Liew <110013750+isaacliew156@users.noreply.github.com>
2023-04-14 22:36:25 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cov0jlo9babt.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "liewyishen-employee"
+customregion = "us-east-1"
